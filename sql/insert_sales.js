a17230f3328c5ad53e2c5d3e7e7eb0a2ec29c80c
Lusindiso <lusindiso@projectcodex.co>
2016-09-15 12:02:04 +0200

    sql

--- sql/insert_sales.js
+++ sql/insert_sales.js
@@ -11,6 +11,36 @@ var conn = mysql.createConnection({
   //getting the products from the other file
  conn.query('SELECT * FROM products', function(err, products){
    if(err) throw err;
-console.log(products);
+//console.log(products);
+
+var prod = [];
+
+var files = fs.readFileSync('../files/week1.csv', 'utf8').split('\n');
+  for(var i = 0; i < files.length -1; i++){
+         var data = files[i].split(',');
+        // console.log(data);
+         prod.push(data)
+  }
+
+    var sales = [];
+
+
+  // prod.forEach(function(item){
+
+  for(var i = 0; i < prod.length; i++){
+    prod[i] = prod[i],
+
+    sales.push(
+
+      var date = prod[i][1],
+      var no_sold = prod[i][3],
+      var price = prod[i][4]
+
+})
+    }
+
+
+
+
 
 });
